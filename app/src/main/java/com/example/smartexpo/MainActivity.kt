8a93034917af101add55dedb529bb8b250d5ba60
Dolfina Carballeira <dolfinacg@gmail.com>
2022-09-16 18:36:15 -0300

    Hompage setting

--- app/src/main/java/com/example/smartexpo/MainActivity.kt
+++ app/src/main/java/com/example/smartexpo/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.smartexpo
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+    val db = Firebase.firestore
+    var btn_pos : Button ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_pos=findViewById(R.id.btn_pos)
+        btn_pos!!.setOnClickListener{
+            Toast.makeText(this, "ESTOY CERCA", Toast.LENGTH_SHORT).show()
+        }
     }
+
+    /*private fun setUsers(){
+         Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+         Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+
+    }*/
+
 }
